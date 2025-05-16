8bb237c8db57761e3298e8c78c5c1574e100704b
Sumit sengar <sengarsumit45@gmail.com>
2025-05-16 12:39:09 +0530

    res

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -63,7 +63,7 @@ useEffect(() => {
       },
       {
         name: "second",
-        distance: screenWidth <= 861 ? 110 : 170,
+        distance: screenWidth <= 861 ? 110 : 150,
         color: "green",
         technologies: [
           { name: "Linux", logo: Linuxlogo, speed: 0.5 },
@@ -73,7 +73,7 @@ useEffect(() => {
       },
       {
         name: "Third",
-        distance: screenWidth <= 861 ? 160 : 240,
+        distance: screenWidth <= 861 ? 160 : 200,
         color: "red",
         technologies: [
           { name: "Spring", logo: Springlogo, speed: 0.3 },
@@ -94,7 +94,7 @@ useEffect(() => {
   return (
     <>
         <div className="relative min-h-screen flex flex-col   items-center bg-black text-white px-4">
-        <div className="absolute top-20 left-139 flex items-center justify-center hidden sm:flex">
+        <div className="absolute top-20 left-[20.5rem] flex items-center justify-center hidden sm:flex">
          <h1 className='text-7xl '>S</h1>
              <div className="absolute w-40 h-40 rounded-full border border-[#2A6F4A] "></div>
              <div className="absolute w-35 h-35 rounded-full border border-[#2A6F4A] "></div>
@@ -110,10 +110,10 @@ useEffect(() => {
           Skills & Tools I Master
         </h1>
       </div>
-       <div className="absolute  top-15 right-140 w-1 h-29 bg-blue-500 mx-auto ">
+       <div className="absolute  top-15 right-[20rem] w-1 h-29 bg-blue-500 mx-auto ">
          <div className="absolute left-1/2 bottom-0 transform -translate-x-1/2 w-0 h-0 border-l-8 border-r-8 border-t-12 border-transparent border-t-blue-500"></div>
        </div>
-      <div className='absolute top-45 py-4 sm:py-6 md:py-8 lg:md:py-12 w-full  '>
+      <div className='absolute top-[10rem] py-4 sm:py-6 md:py-8 lg:md:py-12 w-full  '>
      <div className='container mx-auto px-4 sm:px-6'>
       <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,,transparent)]">
       <motion.div className='flex gap-10 flex-none pr-14'
